6e0dbc95069c2a23d0654a608df94238888a6e8b
tiagorv <tiago_valerio_betha@hotmail.com>
2019-12-23 15:48:33 -0300

    Classe Vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		Department obj = new Department(1, "Artigos Esportivos");
-		System.out.println(obj);
+		Seller vendedor = new Seller(21, "Tiago Valério", "tiagorv@teste.com.br", new Date(sdf.parse("12/01/1981").getTime()), 2130.44, obj);
+		System.out.println(vendedor);
 	}
 
 }
